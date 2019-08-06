dee037d58b0ce45a72b93d0db130fed502cb4cb1
Christian Rühl <ruehl@objectix.de>
2019-08-06 15:26:05 +0200

    Refactoring: added book store service.

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,3 +1,4 @@
+import { BookStoreService } from './../shared/book-store.service';
 import { Book } from './../shared/book';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
@@ -11,36 +12,10 @@ export class BookListComponent implements OnInit {
   books: Book[];
   @Output() showDetailsEvent = new EventEmitter<Book>();
 
-  constructor() { }
+  constructor(private bookStoreService: BookStoreService) { }
 
   ngOnInit() {
-    this.books = [
-      {
-        isbn: '332323232233',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
-        published: new Date(2019, 4, 30),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practives - mit NativeScript und NgRx',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch1.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Die Autoren führen Sie mit einem ansprichsvollen Beispielprojekt durch die Welt von Angular...'
-      }, {
-        isbn: '332323234213',
-        title: 'Superstar Angular',
-        authors: ['Christian Rühl', 'Markus Schramm', 'Heiko Barth'],
-        published: new Date(2019, 6, 30),
-        subtitle: 'Profi Know-how für Angular-Entwickler',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/buch2.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Besser geht es nicht: das ultimative Buch zu Angular, geschrieben von den besten Entwicklern der Welt...'
-      }
-    ];
+    this.books = this.bookStoreService.getAll();
   }
 
   showDetails(book: Book) {
---
+++ src/app/shared/book-store.service.ts
@@ -0,0 +1,40 @@
+import { Book } from './book';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookStoreService {
+
+  constructor() { }
+
+  getAll(): Book[] {
+    return [
+      {
+        isbn: '332323232233',
+        title: 'Angular',
+        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Koppenhagen'],
+        published: new Date(2019, 4, 30),
+        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practives - mit NativeScript und NgRx',
+        rating: 5,
+        thumbnails: [{
+          url: 'https://ng-buch.de/buch1.jpg',
+          title: 'Buchcover'
+        }],
+        description: 'Die Autoren führen Sie mit einem ansprichsvollen Beispielprojekt durch die Welt von Angular...'
+      }, {
+        isbn: '332323234213',
+        title: 'Superstar Angular',
+        authors: ['Christian Rühl', 'Markus Schramm', 'Heiko Barth'],
+        published: new Date(2019, 6, 30),
+        subtitle: 'Profi Know-how für Angular-Entwickler',
+        rating: 5,
+        thumbnails: [{
+          url: 'https://ng-buch.de/buch2.jpg',
+          title: 'Buchcover'
+        }],
+        description: 'Besser geht es nicht: das ultimative Buch zu Angular, geschrieben von den besten Entwicklern der Welt...'
+      }
+    ];
+  }
+}
